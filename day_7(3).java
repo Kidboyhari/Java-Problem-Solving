9c69160b676aab0a876fd18a61ab4d7a07379d69
HariKrishna G <kidboyhari@gmail.com>
2026-02-17 13:43:07 +0530

    D7P3

---
+++ day_7(3).java
@@ -0,0 +1,23 @@
+// 
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine().trim();
+        scan.close();
+        
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+        
+        String[] tokens = s.split("[^a-zA-Z]+");
+        
+        System.out.println(tokens.length);
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+    }
+}
